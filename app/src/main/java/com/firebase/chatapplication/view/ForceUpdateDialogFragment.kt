965b4b9d84b2b69d52361e70ec62ef4637430154
Andrew <vereszp@gmail.com>
2019-09-22 21:34:39 +0200

    App distribution link added

--- app/src/main/java/com/firebase/chatapplication/view/ForceUpdateDialogFragment.kt
+++ app/src/main/java/com/firebase/chatapplication/view/ForceUpdateDialogFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.firebase.chatapplication.R
+import com.firebase.chatapplication.openLink
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.dialog_force_update.view.*
 
@@ -22,7 +23,9 @@ class ForceUpdateDialogFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.dialog_force_update, null).apply {
-            btnUpdate.setOnClickListener { dismiss() }
+            btnUpdate.setOnClickListener {
+                context.openLink("https://appdistribution.firebase.dev/app_distro/projects/5d87c82b893cfa5dab18335c")
+            }
         }
     }
 }
